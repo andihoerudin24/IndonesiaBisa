de61e09234b795f23642cca61deaf6eb8360b5fd
andihoerudin24 <andihoerudin24@gmail.com>
2019-02-23 16:03:05 +0700

    tambahan inputan menggunakan jqury

---
+++ app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#exampleFormControlSelect2').select2();
+    $('#exampleFormControlSelect3').select2();
+    $('#exampleFormControlSelect12').select2();
+    $('#exampleFormControlSelect31').select2();
+    $('#exampleFormControlSelect90').select2();
+    $('.uang').mask('000.000.000', {reverse:true});
+    $('.uang1').mask('000.000.000', {reverse:true});
+});
