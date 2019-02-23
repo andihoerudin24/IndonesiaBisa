2359d2a8b585fd6818700f0bff3ccb179a0804e0
andihoerudin24 <andihoerudin24@gmail.com>
2019-02-23 17:14:44 +0700

    revisi inputan

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ $(document).ready(function() {
     $('#exampleFormControlSelect2').select2();
     $('#exampleFormControlSelect3').select2();
     $('#exampleFormControlSelect12').select2();
-    $('#exampleFormControlSelect31').select2();
+    $("#exampleFormControlSelect31").select2({
+        tags: true,
+        tokenSeparators: [',', ' ']
+    })
     $('#exampleFormControlSelect90').select2();
     $('.uang').mask('000.000.000', {reverse:true});
     $('.uang1').mask('000.000.000', {reverse:true});
